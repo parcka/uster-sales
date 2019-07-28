0f0bec470ec51015b724364320f36c42fb5b65f8
Parcka <rmanchon@hotmail.com>
2019-07-28 01:20:24 -0400

    feat:domain objets

--- src/main/java/com/parcka/ustersales/model/Trip.java
+++ src/main/java/com/parcka/ustersales/model/Trip.java
@@ -1,6 +1,22 @@
 package com.parcka.ustersales.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
 public class Trip {
-    
-    
+
+    @Id
+    Long id;
+
+    @ManyToOne
+    Driver driver;
+
+    @ManyToOne
+    Vehicle vehicle;
+
+    Date date;
+
 }
